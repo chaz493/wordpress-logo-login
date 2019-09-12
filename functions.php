58000201071c1fe867abbf484e0065ddcad01aa1
Charlie J <46576526+chaz493@users.noreply.github.com>
2019-09-12 16:18:35 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function my_login_logo_url() {
 add_filter( 'login_headerurl', 'my_login_logo_url' );
 
 function my_login_logo_url_title() {
-    return 'NetForks.com';
+    return 'Your Site Name';
 }
 add_filter( 'login_headertitle', 'my_login_logo_url_title' );
 function my_login_logo() { ?>
@@ -18,7 +18,7 @@ function my_login_logo() { ?>
 			padding-top: 4%;
 		}
         body.login div#login h1 a {
-            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/NetforksLogo2019sm.png);
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/Yourimage.png);
 			background-size: 100%;
             width: 100%;
 			height: 30px;
